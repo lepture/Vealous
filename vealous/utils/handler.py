6d36a0cc98ef475ce932b907b4d6806328b06557
Marvour Young <marvour@gmail.com>
2010-09-19 11:15:30 +0800

    deal with cookie

--- vealous/utils/handler.py
+++ vealous/utils/handler.py
@@ -4,12 +4,23 @@ from google.appengine.ext import webapp
 from Cookie import BaseCookie
 
 class WebHandler(webapp.RequestHandler):
-    def set_cookie(self, key, value='', max_age=None,
+    @property
+    def cookies(self):
+        cookies = self.request.cookies
+        cookies.__setitem__ = self._set_cookie
+        cookies.set_cookie = self._set_cookie
+        cookies.__delitem__ = self._del_cookie
+        cookies.del_cookie = self._del_cookie
+        return cookies
+
+    def _set_cookie(self, key, value='', max_age=None,
                    path='/', domain=None, secure=None, 
                    httponly=False, version=None, comment=None):
         """
         Set (add) a cookie for the response
         """
+        if self.request.environ.get('HTTPS', '').lower() in ['on', 'true']:
+            secure = True
         cookies = BaseCookie()
         cookies[key] = value
         for var_name, var_value in [
@@ -28,16 +39,18 @@ class WebHandler(webapp.RequestHandler):
         header_value = cookies[key].output(header='').lstrip()
         self.response.headers._headers.append(('Set-Cookie', header_value))
 
-    def del_cookie(self, key, path='/', domain=None):
-        self.set_cookie(key, '', path=path, domain=domain, max_age=0)
+    def _del_cookie(self, key, path='/', domain=None):
+        self._set_cookie(key, '', path=path, domain=domain, max_age=0)
 
 
 class test(WebHandler):
     def get(self):
-        cookie = self.request.cookies.get('key', None)
+        cookie = self.cookies.get('key', None)
         if not cookie:
-            self.set_cookie('key', 'hello')
-        self.response.out.write('test')
+            self.cookies['key'] = 'hello'
+        del self.cookies['key']
+        self.cookies.set_cookie('key','test', secure=True)
+        self.response.out.write(cookie)
 
 apps = webapp.WSGIApplication(
     [
