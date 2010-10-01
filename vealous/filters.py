accc9566e646de52428504216d90925844673f6c
Marvour Young <marvour@gmail.com>
2010-10-01 12:15:15 +0800

    fix in filter and slim tpl

--- vealous/filters.py
+++ vealous/filters.py
@@ -22,28 +22,15 @@ def prettytime(value):
     return value
 
 @register.filter
-def humantime(value):
-    if isinstance(value, datetime.datetime):
-        now = datetime.datetime.now()
-        time = now - value
-        days = time.days
-        if days:
-            return '%s days ago' % days
-        seconds = time.seconds
-        if seconds < 10:
-            return 'Just now'
-        if seconds / 3600:
-            return '%i hours ago' % int(seconds/3600)
-        if seconds / 60:
-            return '%i minutes ago' % int(seconds/60)
-        return '%i seconds ago' % seconds
-    return value
-
-@register.filter
 def feedtime(value):
     if isinstance(value, datetime.datetime):
         return value.strftime("%a, %d %b %Y %H:%M:%S GMT")
     return value
+
+@register.filter
+def split(value,arg,num=0):
+    num = int(num)
+    return value.split(arg)[num]
 
 @register.filter
 def gravatar(value, arg='normal'):
